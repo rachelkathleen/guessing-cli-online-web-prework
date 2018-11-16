3be5227573392757ec5d49632b505d6792895ade
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-16 20:08:30 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,10 +6,10 @@ def run_guessing_game
   input = gets.downcase.chomp
   number = rand(1..6).to_s
   case input
-  when "exit"
-    puts "Goodbye!"
   when number
     puts "You guessed the correct number!"
+  when "exit"
+    puts "Goodbye!"
     break
   else
     puts "The computer guessed #{number}."
